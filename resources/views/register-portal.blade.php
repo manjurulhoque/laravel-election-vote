d8d22f18aadb4215a6f37b8d8cd48335410a3396
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-11-02 15:05:57 +0600

    Register portal html added

---
+++ resources/views/register-portal.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-4 col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="180"
+                     xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Image cap"
+                     preserveAspectRatio="xMidYMid slice" role="img"><title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#868e96"/>
+                    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                </svg>
+
+                <div class="card-body text-center">
+                    <a href="#" class="btn btn-primary">Voter Registration</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="180"
+                     xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Image cap"
+                     preserveAspectRatio="xMidYMid slice" role="img"><title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#868e96"/>
+                    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                </svg>
+
+                <div class="card-body text-center">
+                    <a href="#" class="btn btn-primary">Candidate Registration</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-4">
+            <div class="card" style="width: 18rem;">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="180"
+                     xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder: Image cap"
+                     preserveAspectRatio="xMidYMid slice" role="img"><title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#868e96"/>
+                    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                </svg>
+
+                <div class="card-body text-center">
+                    <a href="#" class="btn btn-primary">Party Registration</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/register-portal', [App\Http\Controllers\HomeController::class, 'register_portal'])->name('register.portal');
