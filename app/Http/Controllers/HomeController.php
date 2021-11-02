a9fd2d9d4bb502474d78afef2f8365777f658ca1
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-11-03 00:09:35 +0600

    Register as voter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -23,16 +25,37 @@ class HomeController extends Controller
 
     public function voter_register_portal()
     {
-        return view('register-portal.index');
+        return view('register-portal.voter');
+    }
+
+    public function voter_register_submit(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'role' => ['required', 'string'],
+        ]);
+
+        var_dump($request->get('name'));
+
+        User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+            'role' => 'voter'
+        ]);
+
+        return redirect()->route('login');
     }
 
     public function candidate_register_portal()
     {
-        return view('register-portal.index');
+        return view('register-portal.candidate');
     }
 
     public function party_register_portal()
     {
-        return view('register-portal.index');
+        return view('register-portal.party');
     }
 }
