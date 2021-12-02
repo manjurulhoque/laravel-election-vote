1ed9afc8be0b81512bc03791862666ffb68a6ca5
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-12-02 23:51:30 +0600

    Party middleware created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,6 +81,12 @@
                                     </a>
                                 @endif
 
+                                @if(auth()->user()->role == 'party')
+                                    <a class="dropdown-item" href="{{ route('visions.create') }}">
+                                        Our Manifesto
+                                    </a>
+                                @endif
+
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
