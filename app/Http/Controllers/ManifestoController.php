a34d0bf5277fa0400e2cda4dbffcaa8c8ca94980
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-12-02 23:44:32 +0600

    Added manifesto model, controller and migration

---
+++ app/Http/Controllers/ManifestoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Manifesto;
+use Illuminate\Http\Request;
+
+class ManifestoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Manifesto  $manifesto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Manifesto $manifesto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Manifesto  $manifesto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Manifesto $manifesto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Manifesto  $manifesto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Manifesto $manifesto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Manifesto  $manifesto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Manifesto $manifesto)
+    {
+        //
+    }
+}
