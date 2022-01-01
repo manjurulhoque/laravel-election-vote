32a0585293970afd76bd667878aa4dc4087d7b6e
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2022-01-01 23:04:39 +0600

    Election show and update

--- app/Http/Controllers/ElectionController.php
+++ app/Http/Controllers/ElectionController.php
@@ -50,12 +50,31 @@ class ElectionController extends Controller
 
     public function edit(Election $election)
     {
-        //
+        return view('elections.edit', compact('election'));
     }
 
     public function update(Request $request, Election $election)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+        ]);
+
+        $data = $request->all();
+        if ($request->has('is_active')) {
+            if (Election::where('is_active', true)->where('id', '!=', $election->id)->exists()) {
+                return back()->with('warning', "Can't have multiple active election at same time");
+            }
+            $data['is_active'] = true;
+        } else {
+            $data['is_active'] = false;
+        }
+
+        $election = $election->update($data);
+
+        return redirect(route('elections.index'));
     }
 
     public function destroy(Election $election)
--- resources/views/elections/show.blade.php
+++ resources/views/elections/show.blade.php
@@ -4,13 +4,13 @@
 
     <div class="card">
         <div class="card-header">
-            <p class="float-left">Election {{ $election->title }}</p>
+            <p class="float-left">Election: <b> {{ $election->title }}</b></p>
             @if($election->is_active)
                 <p class="float-right text-primary">Currently active</p>
             @else
                 <p class="float-right text-success">Currently not active</p>
             @endif
-            <a href="{{ route('elections.show', $election->id) }}" class="float-right mr-1 btn btn-sm btn-outline-info">Edit</a>
+            <a href="{{ route('elections.edit', $election->id) }}" class="float-right mr-1 btn btn-sm btn-outline-info">Edit</a>
         </div>
         <div class="card-body">
             <div>{!! $election->description !!}</div>
